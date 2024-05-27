521a1bed63fa9077ef9126d8f461a6c6e11245a0
Vadim Cherepanov <xenos26@gmail.com>
2024-05-27 18:49:25 +0700

    feat: start working for profile page

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,14 @@
+const ProfilePage = () => {
+  return (
+    <>
+      {/* My tickets */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between"></div>
+      </section>
+
+      {/* My organized */}
+    </>
+  );
+};
+
+export default ProfilePage;
