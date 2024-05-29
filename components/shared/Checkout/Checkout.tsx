ff128465764b9f19b11b575550a69f7c905e69f4
Vadim Cherepanov <xenos26@gmail.com>
2024-05-29 23:14:03 +0700

    feat: add checkout component

--- components/shared/Checkout/Checkout.tsx
+++ components/shared/Checkout/Checkout.tsx
@@ -1,42 +1,43 @@
 import { Button } from "@/components/ui/button";
 import { IEvent } from "@/lib/database/models/event.model";
 import { loadStripe } from "@stripe/stripe-js";
-import {
-  EmbeddedCheckoutProvider,
-  EmbeddedCheckout,
-} from "@stripe/react-stripe-js";
-import { useCallback } from "react";
+import { useEffect } from "react";
+import { checkoutOrder } from "@/lib/actions/order.actions";
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
 );
 const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
-  const onCheckout = async () => {
-    console.log("Checkout");
-  };
-  const fetchClientSecret = useCallback(() => {
-    // Create a Checkout Session
-    return fetch("/api/checkout_sessions", {
-      method: "POST",
-    })
-      .then((res) => res.json())
-      .then((data) => data.clientSecret);
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you’re ready."
+      );
+    }
   }, []);
-  const options = { fetchClientSecret };
+  const onCheckout = async () => {
+    const order = {
+      eventTitle: event.title,
+      eventId: event._id,
+      price: event.price,
+      isFree: event.isFree,
+      buyerId: userId,
+    };
+    await checkoutOrder(order);
+  };
 
   return (
-    <>
-      <div id="checkout">
-        <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
-          <EmbeddedCheckout />
-        </EmbeddedCheckoutProvider>
-      </div>
-      <form action={onCheckout} method="POST">
-        <Button type="submit" role="link" size="lg" className="button sm:w-fit">
-          {event.isFree ? "Get ticket" : "Buy ticket"}
-        </Button>
-      </form>
-    </>
+    <form action={onCheckout} method="POST">
+      <Button type="submit" role="link" size="lg" className="button sm:w-fit">
+        {event.isFree ? "Get ticket" : "Buy ticket"}
+      </Button>
+    </form>
   );
 };
 
