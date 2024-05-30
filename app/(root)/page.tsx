19dc25c4502f2b29c4baed12df1796b6b07a9130
Vadim Cherepanov <xenos26@gmail.com>
2024-05-30 18:46:26 +0700

    feat: add search component on main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import Collection from "@/components/shared/Collection/Collection";
+import Search from "@/components/shared/Search/Search";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.actions";
 import Image from "next/image";
@@ -44,7 +45,8 @@ export default async function Home() {
           Trust by <br /> Thousands of Events
         </h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          Search CategoryFilter
+          <Search />
+          CategoryFilter
         </div>
         <Collection
           data={events?.data}
