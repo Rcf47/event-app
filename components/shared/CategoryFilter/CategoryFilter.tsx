9b727a5eff2fcefea8134c824cf0a903514e5926
Vadim Cherepanov <xenos26@gmail.com>
2024-05-30 19:31:35 +0700

    feat: add search through search params in category

--- components/shared/CategoryFilter/CategoryFilter.tsx
+++ components/shared/CategoryFilter/CategoryFilter.tsx
@@ -29,28 +29,24 @@ const CategoryFilter = () => {
 
     getCategories();
   }, []);
-  // useEffect(() => {
-  //   const delayDebounceFn = setTimeout(() => {
-  //     let newUrl = "";
-  //     if (categories) {
-  //       newUrl = formUrlQuery({
-  //         params: searchParams.toString(),
-  //         key: "query",
-  //         value: categories,
-  //       });
-  //     } else {
-  //       newUrl = removeKeysFromQuery({
-  //         params: searchParams.toString(),
-  //         keysToRemove: ["query"],
-  //       });
-  //     }
-  //
-  //     router.push(newUrl, { scroll: false });
-  //   }, 300);
-  //   return () => clearTimeout(delayDebounceFn);
-  // }, [categories, searchParams, router]);
 
-  const onSelectCategory = (category: string) => {};
+  const onSelectCategory = (category: string) => {
+    let newUrl = "";
+    if (category && category !== "All") {
+      newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "category",
+        value: category,
+      });
+    } else {
+      newUrl = removeKeysFromQuery({
+        params: searchParams.toString(),
+        keysToRemove: ["category"],
+      });
+    }
+
+    router.push(newUrl, { scroll: false });
+  };
   return (
     <Select onValueChange={(value: string) => onSelectCategory(value)}>
       <SelectTrigger className="select-field">
